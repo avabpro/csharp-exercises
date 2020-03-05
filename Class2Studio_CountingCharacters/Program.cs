50609f2c5d0fc7ec70c6082354d35b5d8190e8bb
Ava <prokop.ava@gmail.com>
2020-03-04 23:32:45 -0600

    bonus mission

--- Class2Studio_CountingCharacters/Program.cs
+++ Class2Studio_CountingCharacters/Program.cs
@@ -8,13 +8,8 @@ namespace Class2Studio_CountingCharacters
     {
         static void Main(string[] args)
         {
-            /*
-            string LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. " +
-                "Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. " +
-                "Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
-                */
-
-            string LoremIpsum = "Loremm.";
+            Console.WriteLine("Enter some text:");
+            string LoremIpsum = Console.ReadLine();
 
             LetterCount(LoremIpsum);
             Console.ReadLine();
